a128d9ba47d179270e80d01061bc217e58e0fe11
someonecantcode <s-btea@lwsd.org>
2026-02-25 20:32:52 -0800

    complted proj3 minheap

--- src/week7/Heap211.java
+++ src/week7/Heap211.java
@@ -1,68 +1,55 @@
 package week7;
+
 import java.util.ArrayList;
 
 //Winter 2026
-
 public class Heap211 {
 
-    static public ArrayList <Integer> heap = new ArrayList<>();
-    
-    
+    static public ArrayList<Integer> heap = new ArrayList<>();
+
     Heap211() {
-    	heap.add(0);  // must understan why we need add(0)
+        heap.add(0);  // must understan why we need add(0)
     }
 
-   
-   
     // project 3
-          
     int parent(int index) {
         return index / 2;
     }
 
-    
     int leftChild(int index) {
         return index * 2;
     }
 
-    
     int rightChild(int index) {
-        return leftChild(index) +  1;
+        return leftChild(index) + 1;
     }
 
-    
     boolean hasParent(int index) {
         // 0 root leftroot rightroot
         // 0  1      2       3
         return index > 1;
     }
 
-    
     boolean hasLeftChild(int index) {
         return leftChild(index) < heap.size();
     }
 
-    
     boolean hasRightChild(int index) {
         return rightChild(index) < heap.size();
     }
-        
-    
+
     void swap(int a, int b) {
         int aValue = heap.get(a);
         int bValue = heap.get(b);
-    
+
         heap.set(a, bValue);
-        heap.set(b,aValue);
+        heap.set(b, aValue);
 
         System.out.printf("%10s %d<->%d %n", "swap", aValue, bValue);
     }
 
     // 
-    
-    
-
-/////////////////////////////////////////////////
+    /////////////////////////////////////////////////
 
     // CP 16: MISSON 1 
 
@@ -70,109 +57,115 @@ public class Heap211 {
     int peekMin() {
         return heap.get(1);
     }
-    
+
     // end of MISSON 1 
-
-//////////////////////////////////////
-
+    //////////////////////////////////////
 
 
 
-    
     boolean isEmpty() {
         return heap.size() == 1;
     }
 
-    
-    
     void add(int value) {
 
-    	
-/////////////////////////////////////////
+        /////////////////////////////////////////
 
     	// CP 16: MISSON 2 
 
     	
          heap.add(value); // add as rightmost leaf
-        
+
         // end of MISSON 2 
+        ////////////////////////////////////////
 
-////////////////////////////////////////
+       	System.out.println("   heap: " + printHeap());
+        System.out.println("   bubble-up: start");
 
-
-
-       	System.out.println("   heap: " +printHeap());
-       	System.out.println("   bubble-up: start");
-
-
-           	
-       	// project 3 implement bubbling-up here
-        int addedValueindex = heap.size() - 1;
-       	while(hasParent(addedValueindex) && value < heap.get(parent(addedValueindex))) {
-            swap(addedValueindex, parent(addedValueindex));
-            addedValueindex = parent(addedValueindex);
+        // project 3 implement bubbling-up here
+        int addedValueIndex = heap.size() - 1;
+        while (hasParent(addedValueIndex) && value < heap.get(parent(addedValueIndex))) {
+            swap(addedValueIndex, parent(addedValueIndex));
+            addedValueIndex = parent(addedValueIndex);
         }
-       	
+
         System.out.println("   bubble-up: end");
-    	System.out.println("   new heap: " +printHeap());
+        System.out.println("   new heap: " + printHeap());
     }
 
-     
-    
-    int remove() {  
-    	
-    	System.out.println("   heap: " +printHeap());  
-       
+    int remove() { // assume size >= 1;
+        System.out.println("   heap: " + printHeap());
 
-
-/////////////////////////////////////
+        /////////////////////////////////////
 
     	// CP 16: MISSON 3  
 
     	
     	int min = peekMin();  // peek min value by calling peekMin()
-        heap.set(1, heap.get(heap.size()-1)); // move the last node to the first. tip: use one of the ArrayList methods
+        int lastelement = heap.get(heap.size() - 1);
+
+        heap.set(1, lastelement); // move the last node to the first. tip: use one of the ArrayList methods
         System.out.println("   Removed: " + min);
-        heap.remove(heap.size()-1); // delete the last node from the heap. The heap is reduced.
-        
-    	 // end of MISSON 3 
+        heap.remove(heap.size() - 1); // delete the last node from the heap. The heap is reduced.
+        System.out.print(isEmpty() ? "" : "      last Node(" + lastelement + ") is moved to the root \n");
 
-////////////////////////////////////////
+        // end of MISSON 3 
+        ////////////////////////////////////////
 
+       	System.out.println("   heap: " + printHeap());
+        System.out.println("   bubble-down: start");
 
+        // project 3 implement bubbling-down here
 
-       	System.out.println("   heap: " +printHeap());   
-     	System.out.println("   bubble-down: start");
-     	
-
-        // project 3
-     	/*
-         implement bubbling-down here
-
-        */
-     	
+        bubbleDown();
+        //
         System.out.println("   bubble-down: end");
-    	System.out.println("   new heap: " +printHeap());
-    	
-      
-       return min;
+        System.out.println("   new heap: " + printHeap());
+
+        return min;
     }
-    
+
+    void bubbleDown() {
+        if (isEmpty()) return;
+
+        int removedValueIndex = 1;
+        while(heap.get(removedValueIndex) > heap.get(getMinChildIndex(removedValueIndex))) {
+            int swapIndex = getMinChildIndex(removedValueIndex);
+            
+            swap(removedValueIndex, swapIndex);
+            removedValueIndex = swapIndex;
+        }
+    }
+
+    int getMinChildIndex(int index) {
+        // edgecase. check if there exists even 1 child (left).
+        if (!hasLeftChild(index)) {
+            return index;
+        }
+        // now we know there exists 100% a left child
+        // assume its left. -> only swap to right if there exists right and right is greater.
+
+        int minChild = leftChild(index);
+        if(hasRightChild(index) && (heap.get(rightChild(index)) < heap.get(minChild))) {
+            return rightChild(index);
+        }
+        return minChild;
+    }
 
     //use this method as is (DO NOT CHANGE)
-    public String printHeap(){
-    	
-        StringBuilder result = new StringBuilder("["); 
-        
-        if (heap.size()>1) {
-         result.append(heap.get(1));
+    public String printHeap() {
+
+        StringBuilder result = new StringBuilder("[");
+
+        if (heap.size() > 1) {
+            result.append(heap.get(1));
         }
-        
-        for (int i = 2; i < heap.size(); i++){
+
+        for (int i = 2; i < heap.size(); i++) {
             result.append(", ").append(heap.get(i));
         }
-                     
+
         return result + "]";
     }
-    
+
 }
--- src/week7/myHeap.java
+++ src/week7/myHeap.java
@@ -1,45 +1,42 @@
 package week7;
+
 import java.util.Random;
 
 // Winter 2026
-
 public class myHeap {
+    final static int HOW_MANY_TESTS = 20;
+	final static int RNG = 3; // higher more likely to add
 
-	   final static int HOW_MANY_TESTs = 5;
-	   
-	   public static void main(String[] args) {
-		   
-		   System.out.println("Project 3. MinHeap (Winter 2026)");
-		   		   
-	        Random random = new Random();
-	        Heap211 minHeap = new Heap211();	 	        
-	        
-	        for (int test = 1; test <= HOW_MANY_TESTs; test++) {
-	        	
-	        	System.out.println();        	
-	             int whatAction = random.nextInt(3); 
-				// int whatAction = 1;
-	            
-	            if (whatAction <= 1) { //add
-	            	
-	                // generate a node (random number) to add.
-	                int node = random.nextInt(50); // 0~49
-	              	                
-	  	            System.out.println("Action " + test + ": Add " + node);
-	             
-	                // add node to the heap	                
-	                minHeap.add(node);	                  
-	            } 
-	            
-	            else if (whatAction == 2) { //remove
-	                if (minHeap.isEmpty()) {
-	                    //skip this action, do not count this action
-	                    test = test - 1;	                    
-	                } else {	                	
-	   		             System.out.println("Action " + test+": Remove min");	   		             
-	                     minHeap.remove();		                     
-	                }
-	            }
-	        }
-	    }
-	}
+    public static void main(String[] args) {
+
+        System.out.println("Project 3. MinHeap (Winter 2026) \n");
+
+        Random random = new Random();
+        Heap211 minHeap = new Heap211();
+
+
+
+        for (int test = 1; test <= HOW_MANY_TESTS; test++) {
+            int whatAction = random.nextInt(RNG+1);
+            // int whatAction = 2;
+
+            if (whatAction <= RNG-1) { //add
+                int node = random.nextInt(50); // 0~49
+                System.out.println("Action " + test + ": Add " + node);
+
+                minHeap.add(node);
+            } else if (whatAction == RNG) { //remove
+                if (!minHeap.isEmpty()) {
+                    System.out.println("Action " + test + ": Remove min");
+                    minHeap.remove();
+                } else {
+					test--;
+					continue;
+				}
+            }
+			System.out.println();
+        }
+
+    }
+
+}
